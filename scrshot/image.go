19e8b85f3a62c2ae90c15d693fa8de6790763180
sanity32 <qwe123qqqq@gmail.com>
2024-02-23 16:06:12 +0300

    scrshot Image populated with SaveJpeg method

--- scrshot/image.go
+++ scrshot/image.go
@@ -5,10 +5,15 @@ import (
 	"image"
 	"image/jpeg"
 	"image/png"
+	"os"
 )
 
 type Image struct {
 	Content image.Image
+}
+
+func NewImage(input image.Image) Image {
+	return Image{Content: input}
 }
 
 func (i Image) ToPng() []byte {
@@ -31,8 +36,16 @@ func (i Image) ToBase64Jpg() B64Result {
 	return NewB64ResultFromBytes(i.ToJpeg())
 }
 
-func NewImage(input image.Image) Image {
-	return Image{
-		Content: input,
+func (i Image) SaveJpeg(filename string, q ...int) error {
+	f, err := os.Create(filename)
+	if err != nil {
+		return err
 	}
+	defer f.Close()
+
+	var opts *jpeg.Options = nil
+	if len(q) > 0 {
+		opts = &jpeg.Options{Quality: q[0]}
+	}
+	return jpeg.Encode(f, i.Content, opts)
 }
